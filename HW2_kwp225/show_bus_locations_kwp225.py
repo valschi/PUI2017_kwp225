14debaa6426126ebe9f02287fd9948a339103ab9
Kent Pan <kentpan@Kents-Air.T-mobile.com>
2017-09-23 23:00:18 -0400

    added comments

---
+++ HW2_kwp225/show_bus_locations_kwp225.py
@@ -0,0 +1,9 @@
+# Author: Kent Pan
+##############################
+# Code written to …fill in later
+##############################
+# describe how code functions
+# fill in later
+
+from __future__ import print_function
+import sys
